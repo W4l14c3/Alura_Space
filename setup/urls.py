1ff73a2b6554c416be03b855a6b86de1a2e775d1
W4l14c3 <wallacedias2016@gmail.com>
2024-08-19 16:58:14 -0300

    Templates, agora adicionamos uma pagina index.html e mapeamos ela em settings.templates, configuramos tbm a views e urls.

--- setup/urls.py
+++ setup/urls.py
@@ -1,5 +1,6 @@
 #Essa pagina é responsavel por fazer o roteamento das requisições.
 #Aqui as requisições serão roteadas para os apps responsaveis por exemplo galeria/urls.py que vai rotear para as paginas que serão renderizadas pela view.
+#Exemplo de roteamento: setup.urls recebe a requisição para galeria, como as rotas da galeria estão incluidas ela responde chamando a index da views, que por sua vez renderiza a index.html da templates.
 
 from django.contrib import admin
 from django.urls import path, include
@@ -7,5 +8,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('galeria.urls'))#Incluimos as rotas da galeria.
+    path('', include('galeria.urls')),#Incluimos as rotas da galeria.
 ]
