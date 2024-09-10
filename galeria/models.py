751acf6393aa37a7e7fdc15ef7a0a330724db3ab
W4l14c3 <wallacedias2016@gmail.com>
2024-09-10 16:26:19 -0300

    Banco de dados, Classe Fotografia, mkmigrations, migrate

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Essa classe é um model pois representa uma tabela no banco de dados
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100,  null=False, blank=False)
+    legenda = models.CharField(max_length=150,  null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100,  null=False, blank=False)
+
+    # Função str subscrita que retorna o nome de um campo da classe fotografia.
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
