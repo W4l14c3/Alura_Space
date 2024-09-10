897687dc30589c4283bfd9bf44675714851b2e44
W4l14c3 <wallacedias2016@gmail.com>
2024-09-10 16:01:54 -0300

    Nomes dinâmicos, dicionario na views retorna na chamada da função index e é iterado com um for.

--- galeria/views.py
+++ galeria/views.py
@@ -2,9 +2,18 @@
 from django.shortcuts import render
 
 
+
 #Funções que iram receber a requisição e responder com a pagina requisitada.
 def index(request):
-    return render (request, 'galeria/index.html')
+    #Este dicionario vai ser carregado junto com a pagina index.
+    dados = {
+    1:  {"nome": "Nebulosa de Carina",
+        "legenda": "webbtelescope.org / NASA / James Webb"},
+    2:  {"nome": "Galáxia NGC 1079",
+       "legenda": "nasa.org / NASA / Hubble"}
+    }
+
+    return render (request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
